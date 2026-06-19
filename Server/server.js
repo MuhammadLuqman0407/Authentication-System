c80faba1a586a1d10cfc906f4c26b1b8e505a388
MuhammadLuqman0407 <muhammadluqman8303@gmail.com>
2026-06-19 23:07:21 +0500

    Mongo Connection working

--- Server/server.js
+++ Server/server.js
@@ -1,22 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-// const nodemailer = require("nodemailer");
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+import app from './src/app.js'
 
 
-const cookieParser = require("cookie-parser");
-
-const app = express();
-const port = process.env.PORT || 4000
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({credentials:true}))
-
-app.get('/', (req, res)=> res.send(" Home Route work correctly"))
-
-
-app.listen(port, () => console.log(` Server stated on the PORT :${port}`))
+app.listen(3000, () => {
+    console.log("Server is Running on port 3000")
+})
