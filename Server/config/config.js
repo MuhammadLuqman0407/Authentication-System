b134f02f0e34a67a34e08521b4f9c27d5f355006
MuhammadLuqman0407 <muhammadluqman8303@gmail.com>
2026-06-25 21:59:15 +0500

    add auth apis

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,9 +1,16 @@
 import dotenv from 'dotenv'
 
 dotenv.config();
-
-const config = {
-    MONGO_URI = process.env.MONGO_URI
+if(!process.env.MONGODB_URI){
+    throw new Error(" Mongo DB environent variable is not set now");
 }
 
-export default config
+if(!process.env.JWT_SECRET){
+    throw new Error(" JWT SECRET is not set in environment Variable");
+}
+const config = {
+    MONGO_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET
+}
+
+export default config;
